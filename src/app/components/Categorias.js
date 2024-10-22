c6e753a8917f99b3c6f8fc2a1affc55ba3b67068
Gris Cazolatti <59493366+Cristianjn4@users.noreply.github.com>
2024-10-22 14:55:49 -0300

    <3

--- src/app/components/Categorias.js
+++ src/app/components/Categorias.js
@@ -2,51 +2,53 @@ export function Categorias(){
     return(
         <div className=" w-full h-auto bg-gradient-to-b bg-white align justify-center py-11 gap-8">
 
-            <h3 className="block lg:hidden text-3xl text-gray-900 align text-center mb-8">
+            <h3 className=" lg:hidden text-3xl text-gray-900 align text-center mb-8">
             <strong> Categorias </strong> de Produtos
             </h3>
 
-            <h3 className="hidden md:block text-2xl text-gray-900 md:text-3xl  align text-center mb-8">
-            <strong>Conforto</strong>, <strong> Qualidade </strong> e <strong>Tecnologia</strong> que só a <strong>Orner Quartos</strong> pode oferecer            </h3>
+            <h3 className="hidden text-2xl text-gray-900 md:text-3xl  align text-center mb-8">
+            <strong>Conforto</strong>, <strong> Qualidade </strong> e <strong>Tecnologia</strong> que só a <strong>Orner Quartos</strong> pode oferecer </h3>
             
-            <div className="w-2/3 h-auto  md:flex lg:flex align m-auto gap-8 text-center ">
+            <div className="w-2/3 h-auto flex align m-auto gap-8 text-center ">
                 <div className="bg-transparent w-40 rounded-full m-auto py-3">
                     <a href="/products#colchoes">
-                    <img  src="Ecomax Restonic.png" alt="" className="bg-white w-40 h-40 rounded-full shadow-lg border-2 border-transparent hover:border-blue-600" />
+                    <img  src="Ecomax Restonic.png" alt="" className="bg-white w-40 h-auto rounded-full shadow-lg border-2 border-transparent hover:border-blue-600" />
                     </a>
                     <br/>
-                    <a className=" b-3 font-normal text-black text-center w-auto"> Colchões </a>
+                    <a className="hidden lg:block b-3 font-normal text-black text-center w-auto"> Colchões </a>
                 </div>
 
-                <div className="bg-transparent w-40 rounded-full m-auto py-4">
+                <div className="hidden lg:block bg-transparent w-40 rounded-full m-auto py-4">
                     <a href="/products#camas">
-                    <img src="camas.png" alt="" className="bg-white w-40 h-40 rounded-full shadow-lg border-2 border-transparent hover:border-blue-600" />
+                    <img src="camas.png" alt="" className="bg-white w-40 h-auto rounded-full shadow-lg border-2 border-transparent hover:border-blue-600" />
                     </a>
                     <br/>
-                    <a href="" className=" b-3 font-normal text-black text-center w-auto py-3"> Camas </a>
+                    <a href="" className="hidden lg:block b-3 font-normal text-black text-center w-auto py-3"> Camas </a>
                 </div>
                 
                 <div className="bg-transparent w-40 rounded-full m-auto py-4">
                     <a href="/products#sistdesc">
-                    <img src="sistdesc.png" alt="" className="bg-white w-40 h-40 rounded-full shadow-lg border-2 border-transparent hover:border-blue-600" />
+                    <img src="sistdesc.png" alt="" className="bg-white w-40 h-auto rounded-full shadow-lg border-2 border-transparent hover:border-blue-600" />
                     </a>
-                    <a href="" className="  font-normal text-black text-center w-auto "> Sistemas de Descanso </a>
+                    <br/>
+
+                    <a href="" className="hidden lg:block  font-normal text-black text-center w-auto "> Sistemas de Descanso </a>
                 </div>
 
                 <div className="bg-transparent w-40 rounded-full m-auto py-4">
                     <a href="/products">
-                    <img src="https://aladimdecor.fbitsstatic.net/img/p/kit-2-travesseiros-percal-firme-50x70-suporte-firme-151885/338844.jpg" alt="" className="bg-white w-40 h-40 rounded-full shadow-lg border-2 border-transparent hover:border-gray-400" />
+                    <img src="https://aladimdecor.fbitsstatic.net/img/p/kit-2-travesseiros-percal-firme-50x70-suporte-firme-151885/338844.jpg" alt="" className="bg-white w-40 h-auto rounded-full shadow-lg border-2 border-transparent hover:border-gray-400" />
                     </a>
                     <br/>
-                    <a href="" className=" b-3 font-normal text-black text-center w-auto py-3"> Travesseiros </a>
+                    <a href="" className="hidden lg:block b-3 font-normal text-black text-center w-auto py-3"> Travesseiros </a>
                 </div>
 
-                <div className=" bg-transparent w-40 rounded-full m-auto py-4">
+                <div className="hidden lg:block bg-transparent w-40 rounded-full m-auto py-4">
                     <a href="/products">
-                    <img src="https://cdn.sistemawbuy.com.br/arquivos/dc7db93e78bf0dd8806d89ead6f85ac6/produtos/6598af8fb402c/jogo-de-cama-casal-queen-liso-100-algodao-200-fios-04-pecas-tous-les-jours-branco-6102efd286e58-large-6598af90678a2_mini.jpg" alt="" className="shadow-lg bg-white w-40 h-40 rounded-full border-2 border-transparent hover:border-gray-400" />
+                    <img src="https://cdn.sistemawbuy.com.br/arquivos/dc7db93e78bf0dd8806d89ead6f85ac6/produtos/6598af8fb402c/jogo-de-cama-casal-queen-liso-100-algodao-200-fios-04-pecas-tous-les-jours-branco-6102efd286e58-large-6598af90678a2_mini.jpg" alt="" className="shadow-lg bg-white w-40 h-auto rounded-full border-2 border-transparent hover:border-gray-400" />
                     </a>
                     <br/>
-                    <a href="" className=" b-3 font-normal text-black text-center w-auto py-3"> Acessórios   </a>
+                    <a href="" className="hidden lg:block b-3 font-normal text-black text-center w-auto py-3"> Acessórios   </a>
 
                 </div>
             </div>
--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br" className=" !scroll-smooth">
       <head>
+
       <script src="login.js" type="text/javascript"></script>
       <script src="script.js" type="text/javascript"></script>
 
@@ -28,9 +29,29 @@ export default function RootLayout({ children }) {
         href="https://cdn.jsdelivr.net/npm/tw-elements/css/tw-elements.min.css" />
 
       </head>
+
+      <Script id="gtm" strategy="afterInteractive">
+      {
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KGW7Z4LV');
+      }
+    </Script>
+
       <body className={montserrat.className} >
+
+
+        {/*      <!-- Google Tag Manager (noscript) --> */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KGW7Z4LV"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        {/*<!-- End Google Tag Manager (noscript) --> */}
+
+
         {children}
       <script src="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"></script>
+
       </body>
     </html>
   );
