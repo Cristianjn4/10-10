5e1c6dad94bedeb984ee5ee2f0ca7959040362f4
Gris Cazolatti <59493366+Cristianjn4@users.noreply.github.com>
2024-10-23 13:13:12 -0300

    google-tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script'
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -15,7 +16,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br" className=" !scroll-smooth">
       <head>
-
       <script src="login.js" type="text/javascript"></script>
       <script src="script.js" type="text/javascript"></script>
 
@@ -28,18 +28,22 @@ export default function RootLayout({ children }) {
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/tw-elements/css/tw-elements.min.css" />
 
+        {/*<!-- Google Tag Manager --> */}
+          
+        <Script id="google-tag-manager" strategy="afterInteractive">
+        {
+          function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f)';
+          (window,document,'script','dataLayer','GTM-KGW7Z4LV');
+        }
+        }
+        </Script>
+
+        {/* <!-- End Google Tag Manager -->*/}
+
       </head>
-
-      <Script id="gtm" strategy="afterInteractive">
-      {
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-KGW7Z4LV');
-      }
-    </Script>
-
       <body className={montserrat.className} >
 
 
